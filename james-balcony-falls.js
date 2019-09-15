5357b742752954e2ebb7a8977749542588d20648
ecc521 <42424091+ecc521@users.noreply.github.com>
2019-09-14 20:12:08 -0400

    Add James @ Balcony Falls virtual gauge

---
+++ james-balcony-falls.js
@@ -0,0 +1,24 @@
+if (globalThis.gauges !== undefined) {
+	let james = gauges["02019500"]
+	let maury = gauges["02024000"]
+	
+	let balconyGauge = {cfs: []}
+	
+	//TODO: Make sure that the timestamps add up.
+	let len = Math.max(james.cfs.length, maury.cfs.length)
+	for (let i=0;i<len;i++) {
+		balconyGauge.cfs[i] = {
+			value: james.cfs[i].value + maury.cfs[i].value,
+			dateTime: james.cfs[i].dateTime
+		}
+	}
+	
+	balconyGauge.name = "James @ Buckhanan + Maury @ Buena Vista"
+	balconyGauge
+}
+else {
+	[
+		"02019500", //USGS 02019500 JAMES RIVER AT BUCHANAN, VA
+		"02024000" //USGS 02024000 MAURY RIVER NEAR BUENA VISTA, VA
+	]
+}
